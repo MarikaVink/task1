80d3db99154e879e247f64232e94a4b003596088
marvin <marvin@greeny.cs.tlu.ee>
2016-02-11 12:44:26 +0200

    added task1 file

--- helloworld.php
+++ helloworld.php
@@ -1,1 +1,15 @@
-hey
+<?php
+
+	//variable starts with $
+	//string has to use quotation marks
+	//line ends with semicolon
+	$first_name = "Marika";
+	$last_name = "Vinkmann";
+	
+	//show code to browser
+	//for adding up numbers we use + sign
+	//for adding up strings we use .
+	//space is considered a character, thus written inbetween quotation marks (example below)
+	echo $first_name." ".$last_name;
+
+?>
