09419c3de79b6475c70aa9a35b89a722fac20fe8
marvin <marvin@greeny.cs.tlu.ee>
2016-02-11 12:55:27 +0200

    added changes to task1

--- helloworld.php
+++ helloworld.php
@@ -11,5 +11,21 @@
 	//for adding up strings we use .
 	//space is considered a character, thus written inbetween quotation marks (example below)
 	echo $first_name." ".$last_name;
+	
+	// > greater than
+	// < smaller than
+	// >= greater than or equal
+	// <= smaller than or equal
+	// == equal
+	// != not equal
 
+	$age = 5;
+	//if age is smaller than or equal to 6, then you can't sleep at lunchtime
+	if ($age <=6){
+		//it is true
+		echo "yes, you can sleep at lunchtime";
+	}else{
+		//it is false
+		echo "no, you are at school";
+	}
 ?>
